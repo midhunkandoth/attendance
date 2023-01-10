b7ca9207a37b1cbe0d18f4b223164dd03e52780c
Midhun Kandoth <midhun.kandoth@delta.com>
2023-01-10 22:04:35 +0530

    Added models for app and applied migrations

---
+++ mysite/mysite/README.md
@@ -0,0 +1,5 @@
+- django-admin startproject mysite 
+- python manage.py startapp student
+- python manage.py sqlmigrate student 0001
+ 
+
--- mysite/student/models.py
+++ mysite/student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_years = models.IntegerField()
+    hod = models.CharField(max_length=50)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    joining_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
