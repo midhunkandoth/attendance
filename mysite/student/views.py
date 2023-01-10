2c7a6cfb0f3a13804a4b1999ea30fa650834d352
Midhun Kandoth <midhun.kandoth@delta.com>
2023-01-10 22:18:58 +0530

    Template rendering

--- mysite/student/views.py
+++ mysite/student/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Course
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the student index.")
 
-# Create your views here.
+def courses(request):
+    courses_list = Course.objects.all().order_by('-number_of_years')
+
+    context = {'courses_list': courses_list}
+
+    return render(request, 'student/courses.html', context)
