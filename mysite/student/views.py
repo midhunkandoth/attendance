6a2bcfba13a8e35d74d30148ed59eb3f93eb27f5
Midhun Kandoth <midhun.kandoth@delta.com>
2023-01-16 09:27:26 +0530

    request params

--- mysite/student/views.py
+++ mysite/student/views.py
@@ -8,7 +8,10 @@ def index(request):
     return HttpResponse("Hello, world. You're at the student index.")
 
 def courses(request):
-    courses_list = Course.objects.all().order_by('-number_of_years')
+
+    number_of_years = request.GET.get('number_of_years', 5)
+
+    courses_list = Course.objects.filter(number_of_years__lte=number_of_years).order_by('-number_of_years')
 
     context = {'courses_list': courses_list}
 
